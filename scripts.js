1dd6cb0830a1e195d5eabc112aece3da62748451
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2019-04-09 13:00:18 +0300

    Отрефакторили, чтобы было удобнее читать

--- scripts.js
+++ scripts.js
@@ -8,25 +8,22 @@ function startTimer(e) {
   e.preventDefault()
 
   const input = e.target.querySelector('input')
-
   input.disabled = true
+
   progress.style.width = '100vw'
   progress.style.transitionDuration = WORK_TIME + 'ms'
 
   setTimeout(function () {
     input.disabled = false
     input.focus()
+    input.value = ''
+
     progress.style.transitionDuration = ''
     progress.style.width = ''
 
-    // listItem === <li></li>
     const listItem = document.createElement('li')
-
     listItem.innerText = input.value
-    // Добавляем внутрь <ul></ul> новый элемент
     tasks.appendChild(listItem)
-
-    input.value = ''
   }, WORK_TIME)
 }
 
