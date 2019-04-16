180f966c5de4317eeb643b2415d6191fa0ed93cc
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2019-04-16 12:47:52 +0300

    Переключение разделов

--- 02/scripts.js
+++ 02/scripts.js
@@ -6,7 +6,7 @@ const timerLink = document.querySelector('#timer-link')
 const statsLink = document.querySelector('#stats-link')
 const timerSection = document.querySelector('#timer-section')
 const statsSection = document.querySelector('#stats-section')
-
+const footerImg = document.querySelector('footer > img')
 
 switchTo25.onclick = function handleTimeSwitch() {
   if (switchTo25.classList.contains('is-active')) {
@@ -27,13 +27,25 @@ switchTo5.onclick = function handleTimeSwitch() {
 }
 
 timerLink.onclick = function () {
+  if (timerLink.classList.contains('is-active')) {
+    return
+  }
   statsSection.style.display = 'none'
   timerSection.style.display = 'block'
+  timerLink.classList.toggle('is-active')
+  statsLink.classList.toggle('is-active')
+  footerImg.src = 'img/timer-section.svg'
 }
 
 statsLink.onclick = function () {
+  if (statsLink.classList.contains('is-active')) {
+    return
+  }
   timerSection.style.display = 'none'
   statsSection.style.display = 'block'
+  statsLink.classList.toggle('is-active')
+  timerLink.classList.toggle('is-active')
+  footerImg.src = 'img/stats-section.svg'
 }
 
 form.onsubmit = function startTimer(e) {
