d9bda19535ee43f3016ad8e70fb9e111dd48fb7f
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2019-04-16 11:53:06 +0300

    Добавили выблор времени

--- 02/scripts.js
+++ 02/scripts.js
@@ -1,9 +1,32 @@
 const form = document.querySelector('form')
+const switchTo25 = document.querySelector('.switch.is-left')
+const switchTo5 = document.querySelector('.switch.is-right')
+let taskTime = 5000
+
+switchTo25.onclick = function handleTimeSwitch() {
+  if (switchTo25.classList.contains('is-active')) {
+    return
+  }
+  switchTo25.classList.toggle('is-active')
+  switchTo5.classList.toggle('is-active')
+  taskTime = 5000
+}
+
+switchTo5.onclick = function handleTimeSwitch() {
+  if (switchTo5.classList.contains('is-active')) {
+    return
+  }
+  switchTo5.classList.toggle('is-active')
+  switchTo25.classList.toggle('is-active')
+  taskTime = 1000
+}
 
 form.onsubmit = function startTimer(e) {
   e.preventDefault()
 
   const input = e.target.querySelector('input[type=text]')
 
-  console.log(input.value)
+  setTimeout(function () {
+    console.log(input.value)
+  }, taskTime)
 }
